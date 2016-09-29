eaa262acac275be7c51c35a3ea63d5a9c289b69f
Ben Limmer <ben@benlimmer.com>
2016-09-29 09:23:29 -0600

    Rework all to use main id -> widget fetch.

--- app/routes/index.js
+++ app/routes/index.js
@@ -23,40 +23,43 @@ export default Ember.Route.extend({
   model(params) {
     let store = this.store;
 
+    let finderRequest = 'widget-finder';
     if (params.filterByIdMod10) {
-      return this.get('ajax').request('widget-finder?filter=mod10').then((res) => {
-        this._setTotal(res.meta.total);
-
-        let page = params.page;
-        let limit = this.get('limit');
-
-        let offset = limit * (page - 1);
-
-        let pageOfIds = res.ids.slice(offset, limit + offset);
-
-        let toRequest = pageOfIds.reject((id) => {
-          return store.hasRecordForId('widget', id);
-        });
-
-        return this.store.query('widget', {
-          ids: toRequest
-        }).then(() => {
-          let widgets = Ember.A([]);
-          pageOfIds.forEach(function(id) {
-            widgets.pushObject(store.peekRecord('widget', id));
-          });
-
-          return widgets;
-        });
-      });
-    } else {
-      return store.query('widget', {
-        limit: this.get('limit'),
-        page:  params.page,
-      }).then((res) => {
-        this._setTotal(res.meta.total);
-        return res;
-      });
+      finderRequest += '?filter=mod10';
     }
+
+    return this.get('ajax').request(finderRequest).then((res) => {
+      this._setTotal(res.meta.total);
+
+      let page = params.page;
+      let limit = this.get('limit');
+
+      let offset = limit * (page - 1);
+
+      let pageOfIds = res.ids.slice(offset, limit + offset);
+
+      // I'd like to wrap this logic up into a store method
+      let toRequest = pageOfIds.reject((id) => {
+        return store.hasRecordForId('widget', id);
+      });
+
+      let promise;
+      if (toRequest.length > 0) {
+        promise = this.store.query('widget', {
+          ids: toRequest
+        });
+      } else {
+        promise = Ember.RSVP.resolve();
+      }
+
+      return promise.then(() => {
+        let widgets = Ember.A([]);
+        pageOfIds.forEach(function(id) {
+          widgets.pushObject(store.peekRecord('widget', id));
+        });
+
+        return widgets;
+      });
+    });
   }
 });
--- mirage/config.js
+++ mirage/config.js
@@ -1,18 +1,23 @@
 export default function() {
 
   this.get('/widget-finder', function(db, request) {
+    let widgets = db.widgets.all().models;
+
+    let filtered;
     if (request.queryParams.filter === 'mod10') {
-      let widgets = db.widgets.all().models;
-      let filtered = widgets.filter(function(widget) {
+      filtered = widgets.filter(function(widget) {
         return widget.id % 10 === 0;
       });
-      return {
-        ids: filtered.mapBy('id'),
-        meta: {
-          total: filtered.length
-        }
-      };
+    } else {
+      filtered = widgets;
     }
+
+    return {
+      ids: filtered.mapBy('id'),
+      meta: {
+        total: filtered.length
+      }
+    };
   });
 
   this.get('/widgets/:id');
@@ -22,19 +27,6 @@ export default function() {
     limit = +limit;
     page = +page;
 
-    if (ids) {
-      console.log('fetching with ids: ');
-      console.dir(ids);
-      return db.widgets.find(ids);
-    } else {
-      let requestedWidgets = db.widgets.all().models;
-      let offset = limit * (page - 1);
-      return {
-        widgets: requestedWidgets.slice(offset, limit + offset),
-        meta: {
-          total: requestedWidgets.length
-        }
-      };
-    }
+    return db.widgets.find(ids);
   });
 }
